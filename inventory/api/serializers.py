898da01a723c359d974422733de80d3d39b6d15a
First_Encounter <needenergy27@gmail.com>
2023-08-29 21:22:42 +0700

    Added: other endpoints

--- inventory/api/serializers.py
+++ inventory/api/serializers.py
@@ -15,3 +15,39 @@ class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Place
         fields = ["id", "name", "address", "created_date"]
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Group
+        fields = ["place", "comment"]
+
+
+class NomenclatureTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.NomenclatureType
+        fields = []
+
+
+class NomenclatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Nomenclature
+        fields = ["type", "characteristics"]
+
+
+class NomenclatureInstanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.NomenclatureInstance
+        fields = ["nomenclature", "group", "quantity", "comment"]
+
+
+class CharacteristicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Characteristic
+        fields = ["type"]
+
+
+# class CharacteristicValueSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = models.CharacteristicValue
+#         fields = ["value", "characteristic", "nomeclature"]
--- inventory/api/views.py
+++ inventory/api/views.py
@@ -2,10 +2,48 @@ from core import models
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .serializers import PlaceSerializer
+from .serializers import (
+    PlaceSerializer,
+    GroupSerializer,
+    NomenclatureTypeSerializer,
+    NomenclatureSerializer,
+    NomenclatureInstanceSerializer,
+    CharacteristicSerializer,
+    # CharacteristicValueSerializer,
+)
 
 
 # Create your views here.
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = models.Place.objects.all()
     serializer_class = PlaceSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = models.Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class NomenclatureTypeViewSet(viewsets.ModelViewSet):
+    queryset = models.NomenclatureType.objects.all()
+    serializer_class = NomenclatureTypeSerializer
+
+
+class NomenclatureViewSet(viewsets.ModelViewSet):
+    queryset = models.Nomenclature.objects.all()
+    serializer_class = NomenclatureSerializer
+
+
+class NomenclatureInstanceViewSet(viewsets.ModelViewSet):
+    queryset = models.NomenclatureInstance.objects.all()
+    serializer_class = NomenclatureInstanceSerializer
+
+
+class CharacteristicViewSet(viewsets.ModelViewSet):
+    queryset = models.Characteristic.objects.all()
+    serializer_class = CharacteristicSerializer
+
+
+# class CharacteristicValueViewSet(viewsets.ModelViewSet):
+#     queryset = models.CharacteristicValue.objects.all()
+#     serializer_class = CharacteristicValueSerializer
